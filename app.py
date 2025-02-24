2d3ad2d4115d40e33502e0f1a84391c404271c78
Federico Giozzi <fedegiozzi@gmail.com>
2025-02-24 16:46:59 -0300

    cambios

--- app.py
+++ app.py
@@ -2,14 +2,11 @@ from flask import Flask
 from flask import render_template,redirect,request,Response,session
 from flask_mysqldb import MySQL,MySQLdb
 
-app = Flask(__name__,template_folder="")
+app = Flask(__name__, template_folder="template")
 
 @app.route("/")
-
 def home():
-    return render_template("template/index.html")
-
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.secret_key = "123456789"
-    app.run(debug=True,host="0.0.0.0",port=5000,threaded=True)
+    app.run(debug=True)
