1ffa62187a0f9733c0dcd23eb406c6a1d26590e2
Federico Giozzi <fedegiozzi@gmail.com>
2025-02-24 16:30:42 -0300

    cambios

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__,template_folder="")
 @app.route("/")
 
 def home():
-    return render_template("template/index.html")
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
